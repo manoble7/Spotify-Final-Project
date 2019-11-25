8195928a86d31c54e0c568886a569252325a5175
manoble7 <54915974+manoble7@users.noreply.github.com>
2019-11-24 22:53:06 -0600

    Add files via upload

--- final_project_master_code.py
+++ final_project_master_code.py
@@ -6,3 +6,28 @@
 @author: madelinenoble
 """
 
+import spotipy
+import spotipy.oauth2 as oauth2
+import playlist_dictionary
+
+
+def BPM():
+    credentials = oauth2.SpotifyClientCredentials(client_id="ec9bf5bbdcda4e3ebb4e5b3fe719f1ea",client_secret="2a0aede0c27246b19dff50617b4723b4")
+    token = credentials.get_access_token()
+    spotify = spotipy.Spotify(auth=token)
+    
+    Mtype = 'EDM'
+    #Mtype = input("What type of music do you want? ")
+    #BPM = input("What pace do you want? ")
+    
+    type_uri = playlist_dictionary.get_playlist_ID(Mtype)
+#    all_songs = spotify.get(type_uri)
+    tracks = spotify.user_playlist_tracks('Spotify', playlist_id=type_uri, fields=None, limit=100, offset=0, market=None)
+    print(type(tracks))
+    audio = spotify.audio_analysis(tracks[1]['id'])
+
+    
+
+
+if __name__ == "__main__":
+    BPM()
--- playlist_dictionary.py
+++ playlist_dictionary.py
@@ -3,18 +3,21 @@ import spotipy
 # Create dictionary with playlists to access given a particular genre parameter.
 
 # These are all playlists made by Spotify and updated fairly regularly (?)
-playlists = {
-    'Classic Rock': spotify:playlist:37i9dQZF1DWXRqgorJj26U # Rock Classics
-    'Pop': spotify:playlist:37i9dQZF1DXcBWIGoYBM5M # Todays Top Hits
-    'R&B': spotify:playlist:37i9dQZF1DX4SBhb3fqCJd # Are & Be
-    'Rap': spotify:playlist:37i9dQZF1DX0XUsuxWHRQd # RapCaviar
-    'Jazz': spotify:playlist:37i9dQZF1DWVqfgj8NZEp1 # Coffee Table Jazz
-    'Country': spotify:playlist:37i9dQZF1DX1lVhptIYRda # Hot Country
-    'EDM': spotify:playlist:37i9dQZF1DXaXB8fQg7xif # Dance Party
-    'HipHop': spotify:playlist:37i9dQZF1DWY4xHQp97fN6 # Get Turnt
-    'Calm': spotify:playlist:37i9dQZF1DX1s9knjP51Oa # Calm Vibes
-    'Indie Rock': spotify:playlist:37i9dQZF1DX2Nc3B70tvx0 # Ultimate Indie
-    'Instrumental': spotify:playlist:37i9dQZF1DWWQRwui0ExPn # Lo-fi Beats
-
-}
+def get_playlist_ID(key):
+    
+    playlists = {
+        'Classic Rock': 'spotify:playlist:37i9dQZF1DWXRqgorJj26U',  # Rock Classics
+        'Pop': 'spotify:playlist:37i9dQZF1DXcBWIGoYBM5M',  # Todays Top Hits
+        'R&B': 'spotify:playlist:37i9dQZF1DX4SBhb3fqCJd',  # Are & Be
+        'Rap': 'spotify:playlist:37i9dQZF1DX0XUsuxWHRQd',  # RapCaviar
+        'Jazz': 'spotify:playlist:37i9dQZF1DWVqfgj8NZEp1',  # Coffee Table Jazz
+        'Country': 'spotify:playlist:37i9dQZF1DX1lVhptIYRda',  # Hot Country
+        'EDM': 'spotify:playlist:37i9dQZF1DXaXB8fQg7xif',  # Dance Party
+        'HipHop': 'spotify:playlist:37i9dQZF1DWY4xHQp97fN6',  # Get Turnt
+        'Calm': 'spotify:playlist:37i9dQZF1DX1s9knjP51Oa',  # Calm Vibes
+        'Indie Rock': 'spotify:playlist:37i9dQZF1DX2Nc3B70tvx0',  # Ultimate Indie
+        'Instrumental': 'spotify:playlist:37i9dQZF1DWWQRwui0ExPn'  # Lo-fi Beats
+    }
+        
+    return playlists[key]
 
